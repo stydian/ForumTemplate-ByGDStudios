08ac2e61a97caeb0cb7068ed6ce64716cd5d83f4
Gabe Diaz <145076565+stydian@users.noreply.github.com>
2023-09-24 04:58:37 -0500

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,13 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var navUserIcon = document.querySelector(".nav-user-icon");
+
+function settingsMenuToggle() {
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+
+
+
+
+
+
+
